30282a9f5f67b8ee54a3252e0e8d900bbc011946
montisdesign <montisdesign@users.noreply.github.com>
2021-03-18 19:11:40 +0100

    first

--- src/components/NextDays.jsx
+++ src/components/NextDays.jsx
@@ -4,7 +4,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/NextDays.css";
 import {
-	HashRouter as Router,
+	BrowserRouter as Router,
 	Switch,
 	Route,
 	Link,
